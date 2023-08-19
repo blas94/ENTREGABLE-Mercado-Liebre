599d7a1343916ccc2a72b8f8dca9fef24fbcc5b9
blas94 <blasoccofer5@gmail.com>
2023-08-19 20:11:30 -0300

    actualización

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
 const express = require ('express');
 const path = require('path');
-const publicPath = path.resolve(__dirname, '../public')
+
 const app = express();
 
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, "../public")));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor andando en el puerto 3000');
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor andando en el puerto ${PORT}`);
 });
 
 app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname, 'views/home.html'));
 })
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/register.html"));
+  });
+  
+  app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/login.html"));
+  });
